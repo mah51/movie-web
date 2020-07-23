dcfd1b5837fce9f8aaf98dd0c2a19c0d449ca4aa
mah51 <mah51@kent.ac.uk>
2020-07-23 20:22:24 +0100

    Loads of stuff

--- pages/movie.js
+++ pages/movie.js
@@ -119,8 +119,7 @@ export default function Movie({data, author}) {
                                     ) : (
                                     <svg xmlns="http://www.w3.org/2000/svg"
                                          className="w-full h-full rounded-full" width="44" height="44"
-                                         viewBox="0 0 24 24" stroke-width="1.5" stroke="#cbd5e0" fill="none"
-                                         stroke-linecap="round" stroke-linejoin="round">
+                                         viewBox="0 0 24 24" stroke="#cbd5e0" fill="none">
                                       <path stroke="none" d="M0 0h24v24H0z"/>
                                       <rect x="4" y="4" width="16" height="16" rx="2"/>
                                       <line x1="8" y1="4" x2="8" y2="20"/>
